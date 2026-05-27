3d575400493e8d014d85b86f2b8d5cf5014c9424
Puneeth V <puneethvijayakumar333@gmail.com>
2026-05-27 13:22:07 +0530

    made-correction

--- frontend/src/Hooks/useCommunity.ts
+++ frontend/src/Hooks/useCommunity.ts
@@ -16,7 +16,6 @@ export const useGetSuggestedCommunities = () => {
     queryFn: getSuggestedCommunitiesApi,
     queryKey: ["suggestedCommunities"],
     retry: false,
-    select: (res) => res,
     enabled: !!Auth.currentUser,
   });
 };
